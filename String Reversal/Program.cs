aa74bcc4000e708d4bff427a12f684c73592374d
Sisipo Songo <ssongo@my365-uwc.ac.za>
2020-09-19 21:05:05 +0200

    Add project files.

---
+++ String Reversal/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace String_Reversal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputString = "This text is backwards";
+            string outputString = ReverseString(inputString);
+
+            Console.Write("Original String:" + inputString);
+            Console.Write("Reversed String:" + outputString);
+
+            Console.ReadLine();
+        }
+                private  static string ReverseString(string inputString)
+        {
+            string reversedString = "";
+
+            for(int count=inputString.Length - 1; count >= 0; count --)
+            {
+                reversedString += inputString[count];
+            }
+
+            return reversedString;       
+        }
+    }
+}
+ 
